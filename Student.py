a74063bd05511698c6950755a31ee34fe652c0d0
Nedonator1 <61702023+Nedonator1@users.noreply.github.com>
2020-03-04 12:40:07 -0300

    Update Student.py

--- Student.py
+++ Student.py
@@ -8,3 +8,11 @@ class Teacher:
         self.name = name
         self.subject = subject
         self.difficulty = difficulty
+MathTeacher = Teacher("Ms. Bluezard", "Math", 10)
+ReligionTeacher = Teacher("Ms. Rudenccio", "Religion", 2)
+PeTeacher = Teacher("Mr. Souzas", "PE", 1)
+EnglishTeacher = Teacher("Ms. Scriber", "English", 9)
+PortugueseTeacher = Teacher("Ms. Duart", "Portugese", 8)
+SpanishTeacher = Teacher("Ms. Hering", "Spanish", 5)
+WssTeacher = Teacher("Mr. Brabo-sa", "WSS", 7)
+HistoryTeacher = ("Ms. Daphone", "History", 6)
